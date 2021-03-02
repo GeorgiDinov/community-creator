6910e93dca84dee46076dd2a29af74f231875aee
Georgi Dinov <georgidinov1@abv.bg>
2021-03-02 18:04:13 +0200

    Initial Commit

---
+++ web/src/test/java/com/georgidinov/web/WebMainTest.java
@@ -0,0 +1,14 @@
+package com.georgidinov.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebMainTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
